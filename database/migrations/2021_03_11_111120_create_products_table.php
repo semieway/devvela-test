db36038bd3ca6e04301cb007d5267f59aecc60dd
Alex Sirotin <semieway@gmail.com>
2021-03-11 16:32:27 +0500

    Create product model & migration

---
+++ database/migrations/2021_03_11_111120_create_products_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->integer('product_id');
+            $table->string('title');
+            $table->text('description')->nullable();
+            $table->float('rating')->nullable();
+            $table->integer('price');
+            $table->integer('inet_price')->nullable();
+            $table->string('image')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
