5c4dad757f8192da85765b03e467a7a9fd3be282
Alex Sirotin <semieway@gmail.com>
2021-03-11 19:07:43 +0500

    Create product routes and templates

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,9 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    //
+    public function index()
+    {
+        return view('products.index');
+    }
+
+    public function show(Product $product) {
+        return view('products.show', ['product' => $product]);
+    }
 }
