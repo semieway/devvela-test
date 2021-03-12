df77519190c9fa96f209ae05e6872c6c194643ce
Alex Sirotin <semieway@gmail.com>
2021-03-12 09:50:49 +0500

    Add bootstrap css

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -7,8 +7,9 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Laravel</title>
     <link href="{{ asset('css/main.css') }}" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
 </head>
-<body>
+<body class="container">
 @section('content')
 @endsection
 </body>
